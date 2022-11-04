f7a72b30ad1b8cdbae084ed28f60e81985036189
Taís Mello <105865585+mellotais@users.noreply.github.com>
2022-11-03 22:48:46 -0300

    Update Main.java

--- src/cone/Main.java
+++ src/cone/Main.java
@@ -2,15 +2,10 @@ package cone;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Cone c = new Cone(2, 4, 2);
-		System.out.println(c.toString());
-		
-		Tinta t = new Tinta(2);
-		t.setLatas(c.getAreaTotal());
-		t.setPrecoTotal(t.getLatas(), t.getPreco());
-		System.out.println(t.toString());
-
-	}
+		public static void main(String[] args) {
+			
+			Cone c = new Cone(18.7,23.8,2);
+			System.out.println(c);
+		}
 
 }
